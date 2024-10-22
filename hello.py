76d3af3092d8916178feb0e615df0bac2f7282e5
gabriel <biel_fahl@hotmail.com>
2024-10-22 20:05:00 -0300

    atualização aula 4

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -12,13 +12,21 @@ def index():
    now = datetime.utcnow()
    return render_template('index.html', current_time=now)
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario, instituicao):
+    return render_template('user.html', name=name,prontuario=prontuario, instituicao=instituicao)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    user_agent = request.headers.get('User-Agent')  # Informações do navegador
+    remote_ip = request.remote_addr  # IP do usuário
+    host = request.host  # Host da aplicação
+
+    return render_template('contexto.html', name=name, user_agent=user_agent, remote_ip=remote_ip, host=host)
 
 if __name__ == '__main__':
     app.run(debug=True)
